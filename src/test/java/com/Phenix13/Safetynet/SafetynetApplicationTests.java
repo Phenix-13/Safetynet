aa9e189398e2ed02ed758b5b4aee7eb63f3391c4
Phenix-13 <alexis.aujard@gmail.com>
2024-01-26 12:32:55 +0100

    Initial commit

---
+++ src/test/java/com/Phenix13/Safetynet/SafetynetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Phenix13.Safetynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
