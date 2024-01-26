65f6c97387ce4aaef0111ba7958df8b90c2effd1
Phenix-13 <alexis.aujard@gmail.com>
2024-01-26 13:51:12 +0100

    Structure package, class and setter and getter done

---
+++ src/main/java/com/Phenix13/Safetynet/model/Data.java
@@ -0,0 +1,9 @@
+package com.Phenix13.Safetynet.model;
+
+import java.util.List;
+
+public class Data {
+    List<Person> persons;
+    List<FireStation> fireStations;
+    List<MedicalRecord> medicalRecords;
+}
---
+++ src/main/java/com/Phenix13/Safetynet/model/MedicalRecord.java
@@ -0,0 +1,12 @@
+package com.Phenix13.Safetynet.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class MedicalRecord {
+    String firstName;
+    String lastName;
+    String birthdate;
+    List<String> medications;
+    List<String> allergies;
+}
